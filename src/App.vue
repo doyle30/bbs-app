6116013c371f106bfac69125d9db46ba058f2f16
thetia7ght <thetia7ght@naver.com>
2020-07-21 12:47:50 +0900

    Update .babelrc, .editorconfig, and 37 more files...

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header/>
+    <BoardVisual/>
+    <router-view/>
+    <Acodian/>
+    <Gallery/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import BoardVisual from "@/components/BoardVisual"
+import Gallery from "@/components/Gallery"
+import Acodian from "@/components/Acodian"
+import Footer from "@/components/Footer"
+export default {
+  name: "App",
+  components: {
+    Header,
+    BoardVisual,
+    Acodian,
+    Gallery,
+    Footer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
